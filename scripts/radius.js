c0e460dd171e696010c33f90d6bc8a6504c9c6c4
name <you@example.com>
2025-05-11 15:32:46 +0200

    radius

--- scripts/radius.js
+++ scripts/radius.js
@@ -1,17 +1,14 @@
 // Ask the user for the radius to calculate the area of ​​a circle. Find the formula for calculating the area of ​​a circle and the value of PI (to 4 decimal places)
 
 // Ask the user for the radius
-let radio = parseFloat(prompt("Introduce el radio del círculo:"));
+let radius = parseFloat(prompt("Enter the radio of the circle:"));
 
-// Validar que el valor ingresado sea un número positivo
-if (isNaN(radio) || radio <= 0) {
-  alert("Por favor, introduce un número válido y positivo.");
-} else {
-  // Valor de PI con 4 decimales
-  const PI = 3.1416;
+// PI value with 4 decimal places
+let PI = 3.1416;
 
-  // Calcular el área del círculo: A = π * r²
-  let area = PI * Math.pow(radio, 2);
+// Calculate the area of ​​the circle: A = π * r²
+let area = PI * Math.pow(radius, 2);
 
-  // Mostrar el resultado
-  alert("El área del círculo es: " + area.toFixed(4));
+console.log("Area of ​​the circle: ", Math.PI*radius**2); // Calculates and displays the area of ​​a circle to the console
+
+ 
