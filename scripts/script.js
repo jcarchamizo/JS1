814be0cb7390f85624a988ce3ce02bd310d80f13
name <you@example.com>
2025-05-11 13:42:27 +0200

    Data Types Javascript

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+// Comments Javascript
+
+//Line Comment
+
+/****************
+ * 
+ * Multi-line Comment
+ * 
+ */
+
+
+// Variables
+
+let message; // Declaration of a Variable
+message = "Hey World"; // Assigning a Value to the Variable
+
+let number = 5; // Declaration and Assigning of a Variable in a Single Line
+
+console.log(number); // Print Value of Variable Message on Console
+
+
+const PI = 3.14; // Declaration of a Constant
+// PI = 3,14159; // This will generate an Erro because a Constant cannot be Reacted
+
+var name = "Juan Carlos"; // Declaring a variable with var (not recommended in ES6)
+
+// Data types in Javascript
+// 1. number
+let age = 25; // Whole Number
+let height = 1.80; // Decimal Number
+let price = 200_000_000; // So Large Number
+
+// 2. string
+let greeting = "Hi, how are you?"; // Text string
+let word = 'A'; // Single-character string
+let stringNumber = "123"; // String that represents a Number
+let complexChain = `Hi, mi name is ${name} and I am ${age} years`// 
+let concatenation = 
+
+console.log(complexChain); // Print complexChain in Console
+
+// 3. boolean
+let sStudiant = true; // Boolean Value (True or False)
+
+// 4. null
+let nullValue = null; // Absence of value
+
+// 5. Undefined
+let UndefinedValue; // Variable declared but not initialized (undefined)
+
+
+let potatoe = "I am a potatoe"; // Declaration of an uninitialized variable
+console.log('potatoe:', potatoe) // Print null in Console
+thisIsANumber = "I am a potatoe"; // Assigning a Value to the Variable
+console.log('This is a number', thisIsANumber); // Print "I am a potatoe" in Console
+
+// 6. Object
+let person = { // Print Object in Console
+    name: "Juan Carlos",
+    age: 35,
+    city: "Barcelona",
+    isStudiant: true,
+};
+
+console.log(person); // Print Object in Console
+
+let vector = [1,2,3,4,5]; // Declaring to Array (vector)
+console.log(vector); // Print to array in Console
+
+console.log(vector[1]); // Print First Element to Array 1 in Console
+
